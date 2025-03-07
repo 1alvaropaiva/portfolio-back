a017d4e04a9377aac02f49bc9f69331e7b91816e
1alvaropaiva <alvaropaiva.dev@gmail.com>
2025-03-07 17:54:30 -0300

    firebase config

---
+++ src/javascript/firebaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseApp = initializeApp ({
+  apiKey: "AIzaSyA5NakSZglcB646CE3OwakD1e1vGx7aTOU",
+  authDomain: "contactform-2e37b.firebaseapp.com",
+  databaseURL: "https://contactform-2e37b-default-rtdb.firebaseio.com",
+  projectId: "contactform-2e37b",
+  storageBucket: "contactform-2e37b.firebasestorage.app",
+  messagingSenderId: "964120235732",
+  appId: "1:964120235732:web:8147ee32aa6efa1f70f541"
+});
+
+// Initialize Firebase
+export const App = () => {
+    return 0;
+}
